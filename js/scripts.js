bbc00ff7e5e856372eb13effde0bd6ea61d33a66
WStalker <zlokazov.k@gmail.com>
2023-04-16 23:28:20 +0300

    Проверка на переполнение 16.04.2023
    1. Проведена проверка на переполнение главной страницы.
    2. Добавлена анимация слайдера.
    3. Унифицированы стили кнопок.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,65 @@
+// SLIDER
+
+const sliderTabButtons = document.querySelectorAll('.slider-pagination-button');
+const slides = document.querySelectorAll('.slider-item');
+const buttonPrevious = document.querySelector('.previous-slide');
+const buttonNext = document.querySelector('.next-slide');
+
+let counter = 0;
+const maxStep = slides.length - 1;
+slides[counter].classList.add('current-slider-item');
+
+const isFirst = (counter) => {
+  if (counter === 0) {
+    buttonPrevious.setAttribute('disabled', 'disabled');
+  } else {
+    buttonPrevious.removeAttribute('disabled', 'disabled');
+  }
+};
+
+const isLast = (counter) => {
+  if (counter === maxStep) {
+    buttonNext.setAttribute('disabled', 'disabled');
+  } else {
+    buttonNext.removeAttribute('disabled', 'disabled');
+  }
+};
+
+buttonNext.addEventListener('click', () => {
+  slides[counter].classList.remove('current-slider-item');
+  slides[counter].setAttribute('style', 'transform: translate(0%)');
+  sliderTabButtons[counter].classList.remove('slider-pagination-button-current');
+
+  counter++;
+  isLast(counter);
+  isFirst(counter);
+
+  slides[counter].classList.add('current-slider-item');
+  sliderTabButtons[counter].classList.add('slider-pagination-button-current');
+});
+
+buttonPrevious.addEventListener('click', () => {
+  slides[counter].classList.remove('current-slider-item');
+  sliderTabButtons[counter].classList.remove('slider-pagination-button-current');
+
+  counter--;
+  isLast(counter);
+  isFirst(counter);
+
+  slides[counter].classList.add('current-slider-item');
+  slides[counter].removeAttribute('style');
+  sliderTabButtons[counter].classList.add('slider-pagination-button-current');
+});
+
+sliderTabButtons.forEach((button, counter) => {
+  button.addEventListener('click', () => {
+    document.querySelector('.slider-pagination-button-current').classList.remove('slider-pagination-button-current');
+    button.classList.add('slider-pagination-button-current');
+    document.querySelector('.current-slider-item').classList.remove('current-slider-item');
+    slides[counter].classList.add('current-slider-item');
+  });
+});
+
 // TAB-BUTTONS
 
 const tabButtons = document.querySelectorAll('.tab-link');
